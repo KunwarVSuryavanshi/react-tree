87204817e2b798c1e2a2d1d81d141c48a93a3c65
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-03-10 02:15:55 +0530

    why i am doing this

--- src/Components/ReactTree.js
+++ src/Components/ReactTree.js
@@ -3,10 +3,10 @@ import '../ReactTree.css'
 
 export default function ReactTree() {
   const [treeData, setTreeData] = useState([
-    { 
+    {
       value: 'Parent 1',
       children: [
-        { 
+        {
           value: 'Children 1-1',
           children: [
             {
@@ -35,52 +35,57 @@ export default function ReactTree() {
               value: 'Children 1-1-3',
               children: []
             }
-          ] 
+          ]
         },
-        { 
+        {
           value: 'Children 1-2',
-          children: [] 
+          children: []
         },
-        { 
+        {
           value: 'Children 1-3',
-          children: [] 
+          children: []
         }
       ]
-    }, 
-    { 
-      value: 'Parent 2', 
+    },
+    {
+      value: 'Parent 2',
       children: [
-        { 
+        {
           value: 'Children 2-1',
-          children: [] 
+          children: []
         },
-        { 
+        {
           value: 'Children 2-2',
-          children: [] 
+          children: []
         },
-        { 
+        {
           value: 'Children 2-3',
-          children: [] 
+          children: []
         }
       ]
-    }, 
-    { value: 'Parent 3', children: []}, 
-    { value: 'Parent 4', children: []}, 
-    { value: 'Parent 5', children: []}
+    },
+    { value: 'Parent 3', children: [] },
+    { value: 'Parent 4', children: [] },
+    { value: 'Parent 5', children: [] }
   ])
 
   const renderItems = (nodes, parentKey) => {
     return (
       nodes?.map((item, key) => {
         item.nodeInfo = parentKey?.length > 0 ? [...parentKey, key] : [key]
-        item.uniqueKey = `${parentKey?.length > 0 ? `${parentKey},${key}`: `${key}`}`
-        
-        return(
-          <div className='tree-container mrL3'>
-            <div className={`tree-row ${item?.nodeInfo?.length === 1 ? item?.nodeInfo[0] !== 0 ? 'pdT2' : '' : 'pdT2'}`}>
-              <hr className='hr-line'/>
-              <span className={`react-tree-input`}>
+        item.uniqueKey = `${parentKey?.length > 0 ? `${parentKey},${key}` : `${key}`}`
+
+        return (
+          <div className='tree-container pdL2' style={{borderLeft: '2px solid black'}}>
+            <div className={`tree-row ${item?.nodeInfo?.length === 1 ? item?.nodeInfo[0] !== 0 ? 'pdT2' : '' : 'pdT2'}`} style={{ position: 'relative' }}>
+              <span style={{display: 'inline-block', minHeight: '2vh', borderLeft: '2px solid black', top: '1.65vh', position: 'relative' }}>
+              </span>
+              <div style={{display: 'inline-block'}}>
+                <hr style={{ display: 'inline-block', minWidth: '2vw', position: 'relative', top: '0.7vh' }} />
+              </div>
+              <span className={`react-tree-input`} >
                 <input
+                  // style={{ minHeight: '12vh' }}
                   type="text"
                   value={item.value}
                   onChange={(e) => onChangeHandler(e, item)}
@@ -106,17 +111,16 @@ export default function ReactTree() {
   }
 
   const addNode = (values) => {
-    values.children.push({value: '', children: []})
+    values.children.push({ value: '', children: [] })
     setTreeData([...treeData])
   }
 
   const removeNode = (values) => {
-    if(values.uniqueKey.length > 1)
-    {
-      const newKey = values.uniqueKey.slice(0, values.uniqueKey.length -2)
-      removeMapper(newKey, values.uniqueKey.charAt(values.uniqueKey.length -1))
+    if (values.uniqueKey.length > 1) {
+      const newKey = values.uniqueKey.slice(0, values.uniqueKey.length - 2)
+      removeMapper(newKey, values.uniqueKey.charAt(values.uniqueKey.length - 1))
     }
-    else{
+    else {
       treeData.splice(values.uniqueKey, 1)
       setTreeData([...treeData])
     }
@@ -124,13 +128,12 @@ export default function ReactTree() {
 
   const removeMapper = (nkey, uniqueKey, arr = treeData) => {
     arr.map(item => {
-      if(item.uniqueKey === nkey)
-      {
-        item.children.splice(uniqueKey,1)
+      if (item.uniqueKey === nkey) {
+        item.children.splice(uniqueKey, 1)
         setTreeData([...treeData])
         return
       }
-      else if(item.children.length > 0){
+      else if (item.children.length > 0) {
         removeMapper(nkey, uniqueKey, item.children)
       }
     })
@@ -138,10 +141,13 @@ export default function ReactTree() {
 
   return (
     <>
-      <div className='empty-div'>
-      </div>
-      <div className={`react-tree-container`} style={{boxSizing: 'border-box'}}>
-        <div className={`react-tree-row`} >
+      <div className={`react-tree-container pdL2`} style={{ boxSizing: 'border-box' }}>
+        {/* <div className='empty-div'>
+      </div> */}
+        {/* <span className='pdL1'></span>
+        <span style={{ minHeight: '2vh', borderLeft: '2px solid black', minWidth: '2vw' }}>
+        </span> */}
+        <div className={`react-tree-row pdL2`} >
           {renderItems(treeData)}
         </div>
       </div>
